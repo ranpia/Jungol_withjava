8e8e8954e095288d4035ce17086ea108514a21bc
richa <richa@BOOK-IPIOODHQGO>
2024-12-31 15:17:26 +0900

    q613 person class change

--- jungol/src/q613/Person.java
+++ jungol/src/q613/Person.java
@@ -4,4 +4,17 @@ public class Person {
 	private String name;
 	private String school;
 	private int grade;
+	
+	public Person(String name, String school, int grade){
+		this.name=name;
+		this.school=school;
+		this.grade=grade;
+	}
+	
+	public void print() {
+		System.out.println("Name : " + this.name);
+		System.out.println("School : " + this.school);
+		System.out.println("Grade : " + this.grade);
+	}
+	
 }
