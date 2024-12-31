60dc325c589e56a50f9950dc2307f9d0e7f21114
richa <richa@BOOK-IPIOODHQGO>
2024-12-31 11:16:55 +0900

    q555 multiple solved

--- jungol/src/q555/Main.java
+++ jungol/src/q555/Main.java
@@ -10,8 +10,30 @@ public class Main {
 		for (int i=0;i<10;i++) {
 			n=n+sc.next();
 		}
-        sc.close();
 		System.out.print(n);
+		
+		/*
+		// 배열 사용예제
+		char[] ar = new char[10];
+		for (int i=0;i<ar.length;i++) {
+			ar[i]=sc.next().charAt(0);
+		}
+        for (int i=0;i<ar.length;i++) {
+        	System.out.print(ar[i]);
+		}
+		
+		//buffered 사용예쩨
+		throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input[] = br.readLine().split(" "); // 스페이스 기준으로 나누기
+        for(int i=0;i<10;i++){
+            bw.write(String.valueOf(input[i]));
+        }
+        bw.flush();
+		
+		*/
+        sc.close();
 	}
 
 }
