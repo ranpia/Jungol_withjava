fb27a379dc5b1299a11b4c3e2435678b526027de
richa <richa@BOOK-IPIOODHQGO>
2025-01-02 09:08:30 +0900

    q571 commit

---
+++ jungol/src/q571/Main.java
@@ -0,0 +1,23 @@
+package q571;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void printS(int num) {
+		if (num > 1) {
+			for (int i = 0; i < num; i++) {
+				System.out.println("~!@#$^&*()_+|");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		printS(num);
+
+	}
+
+}
