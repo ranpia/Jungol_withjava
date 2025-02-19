64345f97b531caa94b3afd740e0ef150565ce268
김정현 <richard.kim146@gmail.com>
2025-02-19 17:00:34 +0900

    initial

--- jungol/src/q564/Main.java
+++ jungol/src/q564/Main.java
@@ -1,51 +1,28 @@
 package q564;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Character> alphabet = new ArrayList<>();
+        
+        while (sc.hasNext()) {
+            char a = sc.next().charAt(0);
+            if (a >= 'A' && a <= 'Z') {
+                alphabet.add(a);
+            } else {
+                break;
+            }
+        }
+        sc.close();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		ArrayList<Character> alphabet = new ArrayList<>();
-		int i = 0;
+        Map<Character, Integer> asc = new TreeMap<>();
+        
+        for (char ch : alphabet) {
+            asc.put(ch, asc.getOrDefault(ch, 0) + 1);
+        }
 
-		while (true) {
-			char a = sc.next().charAt(0);
-			if (a >= 'A' && a <= 'Z') {
-				alphabet.add(a);
-				i++;
-			}
-			else {
-				break;
-			}
-		}
-		
-		sc.close();
-		
-		Map<Character, Integer> asc = new HashMap<>();
-		
-		for (i = 0; i < alphabet.size(); i++) {
-			if (asc.containsKey(alphabet.get(i))) {
-				int num = asc.get(alphabet.get(i));
-				asc.put(alphabet.get(i), ++num);
-			}
-			else {
-				asc.put(alphabet.get(i), 1);
-			}
-		}
-		
-		List<Character> tempSet = new ArrayList<>(asc.keySet());
-		Collections.sort(tempSet);
-
-		for (char a : tempSet) {
-			System.out.println(a + " : " + asc.get(a));
-		}
-	}
-
+        asc.forEach((key, value) -> System.out.println(key + " : " + value));
+    }
 }
